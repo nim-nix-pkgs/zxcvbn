fc9316d97aa34a17652a88c7f1d0139557eee2a7
hugosenari <hugosenari@gmail.com>
2022-03-12 01:38:23 -0300

    chore: re index zxcvbn

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Nim bindings for the zxcvbn-c password strength estimation library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."zxcvbn-master".url = "path:./master";
+  inputs."zxcvbn-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."zxcvbn-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
